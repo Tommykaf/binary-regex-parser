1fff7e1dee71356c76801ac7defe0afd5499fc20
Tommy KA <poopmckaki@gmail.com>
2020-08-02 00:19:38 +0300

    added dict from mail to test

--- Main.py
+++ Main.py
@@ -87,4 +87,23 @@ def main(path, regex_map, readbuffer = 2**20, buffer_size = 2**13):
 
 
 if __name__ == "__main__":
-    print(main("./flame-qd1a.190821.007/bootloader-flame-c2f2-0.2-5799621.img", {"4642504B0100+": "START", "525C":"525C"}))
+    print(main("./flame-qd1a.190821.007/bootloader-flame-c2f2-0.2-5799621.img", { '5D00008000': 'lzma',
+  '27051956': 'uImage',
+  '18286F01': 'zImage',
+  '1F8B0800': 'gzip',
+  '303730373031': 'cpio',
+  '303730373032': 'cpio',
+  '303730373033': 'cpio',
+  '894C5A4F000D0A1A0A': 'lzo',
+  '5D00000004': 'lzma',
+  'FD377A585A00': 'xz',
+  '314159265359': 'bzip2',
+  '425A6839314159265359': 'bzip2',
+  '04224D18': 'lz4',
+  '02214C18': 'lz4',
+  '1F9E08': 'gzip',
+  '71736873': 'squashfs',
+  '68737173': 'squashfs',
+  '51434454': 'dtb',
+  'D00DFEED': 'fit',
+  '7F454C46': 'elf',"4642504B0100+": "START", "525C":"525C"}))
