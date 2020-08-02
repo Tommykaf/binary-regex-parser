38203a3cb3a76d1637b92d9cffe16b245878f3c2
Tommy KA <poopmckaki@gmail.com>
2020-08-02 15:20:38 +0300

    Docs for main

--- Main.py
+++ Main.py
@@ -19,13 +19,14 @@ def main(path, regex_map, readbuffer = 2**20, buffer_size = 2**13):
     res = []
     fl = read_from_file(path, readbuffer, buffer_size)
 
-    #initialize map from first byte to regexs that it can start with
+    #initialize map from bytes to regexs that can start with it
     initiator_map = {}
     for regex, name in regex_map.items():
         pattern = RegExPattern(regex,name)
         for initiator in pattern.initiators:
             if not initiator_map.get(initiator, False):
                 initiator_map[initiator] = []
+
             initiator_map[initiator].append(pattern)
     
     
@@ -37,12 +38,18 @@ def main(path, regex_map, readbuffer = 2**20, buffer_size = 2**13):
         i = 0
         while i < len(buffer):
             current_byte = buffer[i]
+
+            # Check whteher matchers can be initiatted.
+            # No ttwo pattterns of tthe same kind can overlap.
             if (current_byte in initiator_map.keys()):
                 for pattern in initiator_map[current_byte]:
                     if pattern not in current_patterns:
                         current_patterns.append(pattern)
                         current_matchers.append(RegExMatcher(pattern, bufferNum * buffer_size + i))
 
+            # If a matcher is eitther done or invalid, it should be deleted
+            # along with it's pattern.
+            # If it is done, append it's info to res.
             toDelete = []
             for matcher in current_matchers:
                 if (not matcher.validate(current_byte) or not matcher.isValid()):
@@ -51,11 +58,16 @@ def main(path, regex_map, readbuffer = 2**20, buffer_size = 2**13):
                     res.append(matcher.info())
                     toDelete.append(matcher)
 
+            # Maintenance check, delete all that we can
+            # in addittion, initiate matchers for patterns that have just now failed 
             for matcher in toDelete:
                 current_matchers.remove(matcher)
                 current_patterns.remove(matcher.regex_pattern)
                 
-                if current_byte in initiator_map.keys() and matcher.regex_pattern in initiator_map[current_byte]:
+                if (not matcher.isValid() and 
+                    current_byte in initiator_map.keys() and
+                    matcher.regex_pattern in initiator_map[current_byte]):
+
                     current_patterns.append(matcher.regex_pattern)
                     restartMatcher = RegExMatcher(matcher.regex_pattern, bufferNum * buffer_size + i)
                     current_matchers.append(restartMatcher)
@@ -63,12 +75,13 @@ def main(path, regex_map, readbuffer = 2**20, buffer_size = 2**13):
             
         
             i += 1
+        bufferNum += 1
         try:
             buffer = next(fl)
         except StopIteration as e:
-            break;
-        bufferNum += 1
+            break
     
+    # Final Cleanup
     toDelete = []
     for matcher in current_matchers:
         if (not matcher.isValid()):
